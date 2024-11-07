af0f02f79ae9ee68ff06ebf5bfe12f068f37c4a7
candelanati <candelanati23@gmail.com>
2024-11-07 20:43:42 -0300

    stock funcionando

--- src/components/ButtonAddCart/ButtonAddCart.jsx
+++ src/components/ButtonAddCart/ButtonAddCart.jsx
@@ -8,46 +8,70 @@ const borrarDeCarrito = <svg xmlns="http://www.w3.org/2000/svg" height="24px" vi
 const cantidadCarrito = <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" ><path d="M480-580q-17 0-28.5-11.5T440-620q0-17 11.5-28.5T480-660q17 0 28.5 11.5T520-620q0 17-11.5 28.5T480-580Zm-40-140v-200h80v200h-80ZM280-80q-33 0-56.5-23.5T200-160q0-33 23.5-56.5T280-240q33 0 56.5 23.5T360-160q0 33-23.5 56.5T280-80Zm400 0q-33 0-56.5-23.5T600-160q0-33 23.5-56.5T680-240q33 0 56.5 23.5T760-160q0 33-23.5 56.5T680-80ZM40-800v-80h131l170 360h280l156-280h91L692-482q-11 20-29.5 31T622-440H324l-44 80h480v80H280q-45 0-68.5-39t-1.5-79l54-98-144-304H40Z"/></svg>
 
 function ButtonAddCart ({item}){
-    const {agregarCarrito , borraCarrito  , setItemsCarrito} = useContext(CartContext)
+    const {agregarCarrito , borrarItem  , setItemsCarrito} = useContext(CartContext)
 
     const [cantidad, setCantidad] = useState(0)
-
-        
         function sumaElemento(){
-            setCantidad(cantidad + 1)
             if(cantidad<1){
-               agregarCarrito(item) 
+                agregarCarrito(item)
+                console.log("quedan "+item.stock+ " items")
+                setCantidad(cantidad + 1)
+                item.cantidad++
+                item.stock--
+                setItemsCarrito ((prevItems) => [...prevItems]) // renderiza el context para actualizar los valores
+            }else if(item.stock>=1){
+                console.log("quedan "+item.stock+ " items")
+                setCantidad(cantidad + 1)
+                item.cantidad++
+                item.stock--
+                setItemsCarrito ((prevItems) => [...prevItems]) // renderiza el context para actualizar los valores
+            }else{
+                console.log("no hay mas")
             }
-            item.cantidad++
-            setItemsCarrito ((prevItems) => [...prevItems]) // renderiza el context para actualizar los valores
         }
 
         function restaElemento(){
             item.cantidad--
             setCantidad(cantidad - 1)
             if(item.cantidad == 0) {
-                borraCarrito(item)
+                borrarItem(item)
             }
+            item.stock++
             setItemsCarrito ((prevItems) => [...prevItems]) // renderiza el context para actualizar los valores
         }
 
         function eliminaElemento(){
+            item.stock=item.stock+item.cantidad
             item.cantidad = 0
             setCantidad(0)
-            borraCarrito(item)
+            borrarItem(item)
         }
 
     if(cantidad>=1){
         return(
             <>
                 <div className="suma-y-resta-div">
-                    <div className="cantidad">
-                        <p>{cantidadCarrito} {item.cantidad}</p>
+                    <div className="cantidad-y-stock">
+                        <div className="cantidad">
+                            <p>{cantidadCarrito} {item.cantidad}</p>
+                        </div>
+                        {
+                            item.stock > 0 ?
+                            <>
+                                <button className="suma" onClick={sumaElemento}>+</button>
+                                <button className="resta" onClick={restaElemento}>-</button>
+                                <button className="borrar" onClick={eliminaElemento}>{borrarDeCarrito}</button>
+                                <p className="nota-stock">quedan {item.stock}</p>
+                            </>
+                            :
+                            <>
+                                <button className="resta" onClick={restaElemento}>-</button>
+                                <button className="borrar" onClick={eliminaElemento}>{borrarDeCarrito}</button>
+                                <p className="nota-stock">No hay mas stock</p>
+                            </>
+                        }
                     </div>
-                        <button className="suma" onClick={sumaElemento}>+</button>
-                        <button className="resta" onClick={restaElemento}>-</button>
-                        <button className="borrar" onClick={eliminaElemento}>{borrarDeCarrito}</button>
-                </div>
+               </div>
             </>
         )
     }
